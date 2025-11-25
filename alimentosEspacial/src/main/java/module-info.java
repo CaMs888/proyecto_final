88cf826c3ce2c02ef9daf6548e4c72bcb1432ea5
CaMs888 <scchaves@poligran.edu.co>
2025-11-24 21:04:54 -0500

    javadoc

--- alimentosEspacial/src/main/java/module-info.java
+++ alimentosEspacial/src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module co.edu.poli.alimentosEspacial {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.base;
     
-    opens co.edu.poli.alimentosEspacial.vista to javafx.graphics, javafx.fxml;
+    opens co.edu.poli.alimentosEspacial.vista to javafx.fxml;
     opens co.edu.poli.alimentosEspacial.controlador to javafx.fxml;
     opens co.edu.poli.alimentosEspacial.modelo to javafx.base;
+    opens co.edu.poli.alimentosEspacial.servicios to javafx.base;
     
     exports co.edu.poli.alimentosEspacial.vista;
     exports co.edu.poli.alimentosEspacial.controlador;
     exports co.edu.poli.alimentosEspacial.modelo;
+    exports co.edu.poli.alimentosEspacial.servicios;
 }
